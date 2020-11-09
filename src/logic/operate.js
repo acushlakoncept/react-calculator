417c2046937ef2ee12b8f1b7d4cb3a4b4015b8ea
Uduak Essien <acushla4real@gmail.com>
2020-11-09 17:26:38 +0100

    implement operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,21 @@
 import Big from 'big.js';
 
 export default operate = (numberOne, numberTwo, operation) => {
-
+    num1 = new Big(numberOne)
+    num2 = new Big(numberTwo)
+    if (operation == "-") {
+       return num1 - num2 
+    } 
+    else if (operation == "+") {
+        return num1 + num2 
+    }
+    else if (operation == "X") {
+        return num1 * num2 
+    }
+    else if (operation == "÷") {
+        return num1 / num2 
+    }
+    else if (operation == "%") {
+        return num1/100 || num2/100
+    }
 }
