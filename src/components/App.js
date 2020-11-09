5da6740f2a7ab7939592dfde08b7f4f880f6852b
Uduak Essien <acushla4real@gmail.com>
2020-11-09 14:13:50 +0100

    add logic folder with calculate and operate.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import Big from 'big.js';
 
 export default function App() {
   return (
