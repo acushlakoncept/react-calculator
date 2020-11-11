564c39a7abfe0e13981c38d5d74a4c5d729f1731
Uduak Essien <acushla4real@gmail.com>
2020-11-11 17:00:20 +0100

    implement handleClick function

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
-import calculator from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-export default function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = buttonName => {
+    const objState = this.state;
+    const result = calculate(objState, buttonName);
+    this.setState({
+      total: result.total ? result.total : null,
+      next: result.next ? result.next : null,
+      operation: result.operation ? result.operation : null,
+    });
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <>
+        <Display result={total || next || '0'} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
+
+/**
+ *
+ handleClick = buttonName => {
+    const stateObj = this.state;
+    const output = calculate(stateObj, buttonName);
+    this.setState({
+      total: output.total ? output.total : null,
+      next: output.next ? output.next : null,
+      operation: output.operation ? output.operation : null,
+    });
+  }
+ */
