3b29b78d6c468ac8ebab6efc25798178b89954e0
Uduak Essien <acushla4real@gmail.com>
2020-11-11 17:45:29 +0100

    add clickHandler props to Button

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 export default class App extends Component {
