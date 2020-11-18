f7ce4abed38a9197e5ebc006c1f2292871ee9e47
Uduak Essien <acushla4real@gmail.com>
2020-11-18 12:55:11 +0100

    refactor App.js to use useState hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      totalStatus: true,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [totalStatus, setTotalStatus] = useState(true);
 
-  handleClick = buttonName => {
-    const {
-      total, next, operation, totalStatus,
-    } = this.state;
-    const result = calculate({
-      total, next, operation, totalStatus,
-    }, buttonName);
-    this.setState({
-      total: result.total,
-      next: result.next,
-      operation: result.operation,
-      totalStatus: result.totalStatus,
-    });
-  }
-
-  render() {
-    const { total, next } = this.state;
-    return (
-      <>
-        <Display result={total} next={next} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
+  const handleClick = buttonName => {
+    const result = calculate(
+      {
+        total,
+        next,
+        operation,
+        totalStatus,
+      },
+      buttonName,
     );
-  }
-}
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+    setTotalStatus(result.totalStatus);
+  };
+
+  return (
+    <>
+      <Display result={total} next={next} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
+
+export default App;
+
+// export default class App2 extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//       totalStatus: true
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick = (buttonName) => {
+//     const { total, next, operation, totalStatus } = this.state;
+//     const result = calculate(
+//       {
+//         total,
+//         next,
+//         operation,
+//         totalStatus
+//       },
+//       buttonName
+//     );
+//     this.setState({
+//       total: result.total,
+//       next: result.next,
+//       operation: result.operation,
+//       totalStatus: result.totalStatus
+//     });
+//   };
+
+//   render() {
+//     const { total, next } = this.state;
+//     return (
+//       <>
+//         <Display result={total} next={next} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
