c71fcc542a54e160d9d895f8837476ceb013b614
Uduak Essien <acushla4real@gmail.com>
2020-11-18 15:08:07 +0100

    add nav component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Nav from './Nav';
 
 const Home = () => (
   <div>
+    <Nav />
     <h1>Welcome</h1>
   </div>
 );
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav>
+    <h1>Math Magicians</h1>
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quotes</li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
