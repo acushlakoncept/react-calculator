70165dd07a3069e8fa226f05e238315e88bec8ce
Uduak Essien <acushla4real@gmail.com>
2020-11-19 16:55:11 +0100

    add test for nav links

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 import Button from '../components/Button';
 import Display from '../components/Display';
-import operate from '../logic/operate';
+import Nav from '../components/Nav';
 
+configure({ adapter: new Adapter() });
 it('renders button correctly', () => {
   const tree = renderer.create(<Button name="AC" clickHandler={jest.fn()} />);
   expect(tree).toMatchSnapshot();
@@ -12,4 +15,9 @@ it('renders button correctly', () => {
 it('renders display correctly', () => {
   const tree = renderer.create(<Display result="0" next="" />);
   expect(tree).toMatchSnapshot();
+});
+
+it('contains 3 nav links', () => {
+  const links = shallow(<Nav />).find('li').length;
+  expect(links).toBe(3);
 });
