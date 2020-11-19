fbbf2642fcf89fda31ec8ba6427a97542f94217c
Uduak Essien <acushla4real@gmail.com>
2020-11-19 15:52:26 +0100

    add snapshot test to render display

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Button from '../components/Button';
+import Display from '../components/Display';
+import operate from '../logic/operate';
 
-it('renders correctly', () => {
+it('renders button correctly', () => {
   const tree = renderer.create(<Button name="AC" clickHandler={jest.fn()} />);
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders display correctly', () => {
+  const tree = renderer.create(<Display result="0" next="" />);
   expect(tree).toMatchSnapshot();
 });
