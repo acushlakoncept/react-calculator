985013785050aee02fcfeb62ac1c26557364a79f
Uduak Essien <acushla4real@gmail.com>
2020-11-19 16:59:28 +0100

    add test for button component count

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -4,6 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import Button from '../components/Button';
 import Display from '../components/Display';
+import ButtonPanel from '../components/ButtonPanel';
 import Nav from '../components/Nav';
 
 configure({ adapter: new Adapter() });
@@ -20,4 +21,9 @@ it('renders display correctly', () => {
 it('contains 3 nav links', () => {
   const links = shallow(<Nav />).find('li').length;
   expect(links).toBe(3);
+});
+
+it('contains 19 buttons links', () => {
+  const wrapper = shallow(<ButtonPanel clickHandler={jest.fn()} />).find(Button).length;
+  expect(wrapper).toBe(19);
 });
